8c16869cb65b5b7fbba1d56337730ea0764aa948
kaankotan <kotankaan@proton.me>
2026-08-14 16:00:26 +0200

    feat: Add swagger and adapt controller endpoint

--- backend/BookManager.Api/Controllers/BooksController.cs
+++ backend/BookManager.Api/Controllers/BooksController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace BookManager.Api.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/books")]
 public class BooksController : ControllerBase
 {
     private readonly ISender _sender;
--- backend/BookManager.Api/Program.cs
+++ backend/BookManager.Api/Program.cs
@@ -4,11 +4,21 @@ using BookManager.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapControllers();
 
